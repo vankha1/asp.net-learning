c2bd4c035a2a76fa0f0c072c0f15206168f26e21
vankha1 <vovankha2003@gmail.com>
2024-09-19 22:58:56 +0700

    feat: sorting query

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -47,12 +47,22 @@ namespace api.Repository
         {
             var stocks = _context.Stock.Include(c => c.Comments).AsQueryable();
 
-            if (!string.IsNullOrWhiteSpace(query.CompanyName)){
+            if (!string.IsNullOrWhiteSpace(query.CompanyName))
+            {
                 stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
             }
 
-            if (!string.IsNullOrWhiteSpace(query.Symbol)){
+            if (!string.IsNullOrWhiteSpace(query.Symbol))
+            {
                 stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                }
             }
 
             return await stocks.ToListAsync();
