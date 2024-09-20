d5b989341e6acc4025ef697485455539a7d657e0
vankha1 <vovankha2003@gmail.com>
2024-09-20 22:13:30 +0700

    feat: register account

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,5 +1,6 @@
 using System;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,9 +10,23 @@ public class ApplicationDBContext : IdentityDbContext<AppUser>
 {
     public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
     {
-        
     }
-
     public DbSet<Stock> Stock { get; set; }
     public DbSet<Comment> Comments { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        List<IdentityRole> roles = new List<IdentityRole>{
+            new IdentityRole{
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole{
+                Name = "User",
+                NormalizedName = "USER"
+            },
+        };
+
+        modelBuilder.Entity<IdentityRole>().HasData(roles);
+    }
 }
---
+++ Migrations/20240920150735_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "95c831f7-28a0-485d-90d3-0c5ff0e40d5e", null, "User", "USER" },
+                    { "af4f8e59-a637-4f37-aaf0-c19a8fa16e12", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "95c831f7-28a0-485d-90d3-0c5ff0e40d5e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "af4f8e59-a637-4f37-aaf0-c19a8fa16e12");
+        }
+    }
+}
