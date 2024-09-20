a22d1887b3d705a9d48015dec16c6f02217aab99
vankha1 <vovankha2003@gmail.com>
2024-09-20 22:55:51 +0700

    feat (IMPORTANT): token service & differentiate ROLES and CLAIMS

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using api.Dtos.Account;
+using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,8 +11,10 @@ namespace api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
-        public AccountController(UserManager<AppUser> userManager){
+        private readonly ITokenService _tokenService;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService){
             _userManager = userManager;
+            _tokenService = tokenService;
         }
 
         [HttpPost("register")]
@@ -31,7 +34,11 @@ namespace api.Controllers
                 if (createdUser.Succeeded){
                     var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
                     if (roleResult.Succeeded){
-                        return Ok("User created");
+                        return Ok(new NewUserDto{
+                            UserName = appUser.UserName!,
+                            Email = appUser.Email!,
+                            Token = _tokenService.CreateToken(appUser)
+                        });
                     }
                     return StatusCode(500, roleResult.Errors);
                 }
