099ed2df5dd0e22a9fc07fcdcbeaf9db28a80ae1
vankha1 <vovankha2003@gmail.com>
2024-09-20 23:10:37 +0700

    feat: login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -12,29 +13,75 @@ namespace api.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService){
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
+        {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
+                if (user == null)
+                {
+                    return Unauthorized("Invalid username");
+                }
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+                if (!result.Succeeded)
+                {
+                    return Unauthorized("Username not found or password incorrect");
+                }
+
+                return Ok(new NewUserDto
+                {
+                    UserName = user.UserName!,
+                    Email = user.Email!,
+                    Token = _tokenService.CreateToken(user)
+                });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto){
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
             try
             {
-                if (!ModelState.IsValid){
+                if (!ModelState.IsValid)
+                {
                     return BadRequest(ModelState);
                 }
-                var appUser = new AppUser{
+                var appUser = new AppUser
+                {
                     UserName = registerDto.UserName,
                     Email = registerDto.Email
                 };
 
                 var createdUser = await _userManager.CreateAsync(appUser, registerDto.Password!);
 
-                if (createdUser.Succeeded){
+                if (createdUser.Succeeded)
+                {
                     var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
-                    if (roleResult.Succeeded){
-                        return Ok(new NewUserDto{
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok(new NewUserDto
+                        {
                             UserName = appUser.UserName!,
                             Email = appUser.Email!,
                             Token = _tokenService.CreateToken(appUser)
---
+++ Dtos/Account/LoginDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos.Account
+{
+    public class LoginDto
+    {
+        [Required]
+        public required string UserName { get; set; }
+        [Required]
+        public required string Password { get; set; }
+    }
+}
