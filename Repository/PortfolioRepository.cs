c837e7c24ef91f6354a605e279f4cce7a444267f
vankha1 <vovankha2003@gmail.com>
2024-09-21 10:13:49 +0700

    chore: delete portfolio

--- Repository/PortfolioRepository.cs
+++ Repository/PortfolioRepository.cs
@@ -20,6 +20,20 @@ namespace api.Repository
             return portfolio;
         }
 
+        public async Task<Portfolio?> DeleteAsync(AppUser appUser, string symbol)
+        {
+            var portfolio = await _context.Portfolios.FirstOrDefaultAsync(x => x.AppUserId == appUser.Id && x.Stock.Symbol.ToLower() == symbol.ToLower());
+            
+            if (portfolio == null){
+                return null;
+            }
+
+            _context.Portfolios.Remove(portfolio);
+            await _context.SaveChangesAsync();
+
+            return portfolio;
+        }
+
         public async Task<List<Stock>> GetUserPortfolio(AppUser user)
         {
             return await _context.Portfolios.Where(u => u.AppUserId == user.Id).Select(stock => new Stock
